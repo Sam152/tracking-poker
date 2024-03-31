9644f48bbd4cad743685022adbff71a1309c21e0
Sam <54m152@gmail.com>
2024-03-31 22:11:05 +0800

    Refactor arrow classification

--- frame-analysis/src/stats/up-down/classifyUpDown.test.ts
+++ frame-analysis/src/stats/up-down/classifyUpDown.test.ts
@@ -3,13 +3,11 @@ import path from "path";
 import { classifyUpDown } from "./classifyUpDown";
 
 describe("extractUpDownFromImage", () => {
-    for (const arrowFilename of fs.readdirSync(path.resolve(__dirname, "__fixtures__/arrows"))) {
+    for (const arrowFilename of fs.readdirSync(path.resolve(__dirname, "__fixtures__"))) {
         const classification = arrowFilename.indexOf("up") !== -1 ? 1 : -1;
 
         test(`classifying ${arrowFilename} as ${classification === 1 ? "up" : "down"}`, async () => {
-            const arrow = fs.readFileSync(
-                path.resolve(__dirname, "__fixtures__/arrows", arrowFilename),
-            );
+            const arrow = fs.readFileSync(path.resolve(__dirname, "__fixtures__", arrowFilename));
             expect(await classifyUpDown(arrow)).toEqual(classification);
         });
     }
