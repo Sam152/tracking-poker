f5ac6c8d9d2c9ef7607453cfae79925800964568
Sam <54m152@gmail.com>
2024-04-25 13:44:51 +0800

    Increase ingest speed.

--- infra/lib/IngestStack.ts
+++ infra/lib/IngestStack.ts
@@ -89,7 +89,7 @@ export class IngestStack extends Stack {
 
         // Slowly ingest the full legacy corpus over time.
         new event.Rule(this, "legacy-ingest-trigger", {
-            schedule: event.Schedule.cron({ minute: "0/10" }),
+            schedule: event.Schedule.cron({ minute: "0/2" }),
             targets: [
                 new eventTargets.LambdaFunction(ingestLambda, {
                     event: event.RuleTargetInput.fromObject({
