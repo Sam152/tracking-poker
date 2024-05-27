ea1f869a9fd98b7f6138ca1fef75a84a696ddaa8
Sam <54m152@gmail.com>
2024-05-27 16:16:08 +0800

    Add some comments.

--- frame-analysis/src/stats/util/looksLikePlayerName.ts
+++ frame-analysis/src/stats/util/looksLikePlayerName.ts
@@ -9,10 +9,12 @@ export function looksLikePlayerName(candidate: string): boolean {
         return false;
     }
 
-    // Detect incorrectly classified artefacts in the image.
+    // Detect incorrectly classified artefacts in the image, this can be things from ad-text, supporting words or
+    // individual characters coming from random shapes or textures in the image.
     if (candidate.match(/^VPIP$|^I$|^C$|^O$|^LIVE$|^KUNGJ$|^PRE-FLOP RAISE$|^\d+ BB$/)) {
         return false;
     }
 
+    // Names are uppercase and only have a handful of special characters in them.
     return candidate.match(/^[0-9A-Z-.#' ]+$/) !== null;
 }
